7ad4b375d5cf5b2b1c39789d89f23d67366e396a
ChunQiu203 <156558748+ChunQiu203@users.noreply.github.com>
2026-04-30 23:02:10 +0800

    Delete slayword-main directory

--- slayword-main/scripts/run_modifiers/BaseRunModifier.gd
+++
@@ -1,7 +0,0 @@
-# abstract interface for a run modifier or difficulty
-extends RefCounted
-class_name BaseRunModifier
-
-func run_start_modification() -> void:
-	# any custom logic done at the start of a run
-	pass
--- slayword-main/scripts/validators/card/ValidatorCardTransformableFromDeck.gd
+++
@@ -1,9 +0,0 @@
-# Validator for if the given card can be transformed in the permanent deck or not
-extends BaseValidator
-
-func _validation(card_data: CardData, _action: BaseAction, values: Dictionary[String, Variant]) -> bool:
-	if card_data == null:
-		push_error("No card given")
-		return false
-	
-	return not card_data.card_untransformable_from_deck
